cf673a698684bb1de27f21bd718dcd94085c2433
Bakautov Alexandr <bakautovalex@gmail.com>
2021-07-19 22:39:47 +0700

    - Добавил laravel nova - Добавил авторизацию - Добавил модель сервисов

---
+++ app/Models/Service.php
@@ -0,0 +1,19 @@
+<?php /** @noinspection PhpSuperClassIncompatibleWithInterfaceInspection */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Service extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}
